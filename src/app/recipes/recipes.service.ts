0aa29a3be6dffc0b56c8990ab4a48e128d101a2f
mateusmedeir <mateusmlopestr@gmail.com>
2023-06-12 12:23:15 -0300

    creating ionic-angular-practice

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Schnitzel.JPG/1024px-Schnitzel.JPG',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Spaghettata.JPG/270px-Spaghettata.JPG',
+      ingredients: ['Spaghetti', 'Meat', 'Tomatos']
+    },
+  ];
+  constructor() { }
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string) {
+    return {...this.recipes.find(recipe => recipe.id === recipeId)};
+  }
+
+  deleteRecipe(recipeId: string) {
+    this.recipes = this.recipes.filter(recipe => recipe.id !== recipeId);
+  }
+}
